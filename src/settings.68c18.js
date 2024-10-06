cb6e8b5e6dee9a3b5d8ee822d864ebd6e7a41ec1
Shashank <shashank.chaurasia@gamifystudios.co>
2024-10-06 13:46:09 +0530

    portfolio complete

---
+++ src/settings.68c18.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default","popup"],collisionMatrix:[[true],[false,false]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/gamescene.fire",orientation:"",jsList:[],bundleVers:{internal:"d0832",main:"8162a"}};
